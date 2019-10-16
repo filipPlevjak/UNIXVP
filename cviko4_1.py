3653da84011eb0307d281d2ebd535af1ec66bb88
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2019-10-16 08:43:52 +0200

    Update cviko4_1.py

--- cviko4_1.py
+++ cviko4_1.py
@@ -1,1 +1,23 @@
 #vypocet kombinacneho cisla pomocou faktorialu
+
+#vypocet kombinacneho cisla pomocou faktorialu
+
+def fakt(n):
+	f = 1
+	if n == 0:
+		return 1
+	if n == 1:
+		return 1
+	if n > 1:
+		for i in range(1, n+1):
+			f*=i
+		return f
+		
+def rfakt(n):
+	if n == 0:
+		return 1
+	elif n == 1:
+		return 1
+	else:
+		return n*rfakt(n-1)
+
