deea3d5979ef94f0e2ebbd2b7683d736fee41194
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2019-10-16 09:53:25 +0200

    Update cviko4_1.py

--- cviko4_1.py
+++ cviko4_1.py
@@ -2,6 +2,8 @@
 
 #vypocet kombinacneho cisla pomocou faktorialu
 
+#vypocet kombinacneho cisla pomocou faktorialu
+
 def fakt(n):
 	f = 1
 	if n == 0:
@@ -12,6 +14,9 @@ def fakt(n):
 		for i in range(1, n+1):
 			f*=i
 		return f
+
+def fkt(n):
+	return reduce(lambda x,y: x*y, range(1,n+1))
 		
 def rfakt(n):
 	if n == 0:
@@ -20,4 +25,33 @@ def rfakt(n):
 		return 1
 	else:
 		return n*rfakt(n-1)
+
+
+def fkombi(n,k):
+	return fakt(n)//(fakt(k)* fakt(n-k))
+
+def rkombi(n,k):
+	if k == 1 or k == n:
+		return 1
+	else:
+		return rkombi(n-1, -1)+rkombi(n-1,k)
+
+
+
+d={(0,0):1,(1,0):1,(1,1):1}
+
+def mkombi(n,k):
+	
+	if (n,k) not in d:
+		if k == 0 or k == n:
+			d[n,k]=1
+		else:
+			d[n,k]=mkombi(n-1,k-1)+mkombi(n-1,k)
+	return d[n,k]
+		
+	print (d)
+
+
+ 
+	
 
