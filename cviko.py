fa96f255059df1b05cca57d84aae5a00efe7f5b9
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2019-10-16 08:13:16 +0200

    Update cviko.py

--- cviko.py
+++ cviko.py
@@ -3,6 +3,12 @@
 
 fibo = [0,1]
 
+def fib(n)
+	a,b=0,1
+	if n>1:
+		for i in range(n-1):
+			a,b,a+b
+	return b
 
 def rfib(n)
 	if n == 0
@@ -22,4 +28,7 @@ def mfobi(n):
 		fobi[n] = mfobi(n-1) + mfobi(n-2)
 	return fobi[n]
 
-	
+def mfib(n):
+	if n>len(z)-1:
+		z.append(mfib(n-1)+mfib(n-2))
+	return z[n]
